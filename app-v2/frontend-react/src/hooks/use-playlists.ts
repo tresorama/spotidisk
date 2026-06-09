84e255ad731d6ccb25c064d60ea21387c65a39b1
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-09 17:15:34 +0200

    chore: fe - hooks - usePlaylist - create

--- app-v2/frontend-react/src/hooks/use-playlists.ts
+++ app-v2/frontend-react/src/hooks/use-playlists.ts
@@ -1,9 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { apiClient } from '#/lib/api-client/client';
+import type { DerivedPlaylist } from '#/lib/api-client/types';
 
 export function usePlaylists() {
   return useQuery({
     queryKey: ['playlists'],
     queryFn: () => apiClient.getPlaylists(),
+  });
+}
+
+export function usePlaylist(playlistId: DerivedPlaylist['spotify_id']) {
+  return useQuery({
+    queryKey: ['playlists', playlistId],
+    queryFn: () => apiClient.getPlaylist(playlistId),
   });
 }
