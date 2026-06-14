41b36d4c0b212b7486ba17a9d6aba162a39b3015
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-14 18:56:53 +0200

    chore: be - /playlists - implement/update api endpoints: - GET "/" - list_playlists - GET "/{playlist_id}" - "get_playlist - POST "/{playlist_id}/spotify/refetch" - refetch_spotify_side - POST "/edit-track" - edit_track - POST "/{playlist_id}/track/{track_id}/youtube/auto-search-url" - find_and_set_track_youtube_url - GET "/{playlist_id}/track/{track_id}/disk/get-audio-file" - play_disk_track - POST "/{playlist_id}/track/{track_id}/disk/download" - download_track - POST "/{playlist_id}/track/{track_id}/disk/delete-file" - delete_track

--- app-v2/backend-python/routers/playlist.py
+++ app-v2/backend-python/routers/playlist.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from fastapi import APIRouter, HTTPException
-from models.new import PlaylistDerived
+from fastapi.responses import FileResponse
+from models.new import TrackRaw, PlaylistDerived, PlaylistEditTrackPayload
 from core.singleton.logger import logger
-from core.classes.user_config_api import UserConfigApi
 from core.singleton.config_runtime import config_runtime
-from core.classes.utils_spotify import UtilsSpotify
+from core.classes.user_config_api import UserConfigApi, UserConfigReaderApi
 from core.classes.data_layer_mapper import DataLayerMapper
+from core.classes.utils_spotify import UtilsSpotify
+from core.classes.utils_youtube_fetcher_api import UtilsYoutubeFetcherApi
+from core.classes.utils_track_disk import UtilsTrackDisk
 
 router = APIRouter(prefix="/playlists", tags=["playlists"])
 userConfigApi = UserConfigApi(config_runtime.config_file)
@@ -19,55 +22,228 @@ userConfigApi = UserConfigApi(config_runtime.config_file)
 async def list_playlists():
     """List all saved playlists from config"""
     logger.info("Fetching playlists list")
-    playlists = userConfigApi.config_as_object.saved_playlists
+    playlistsRaw = UserConfigReaderApi.getPlaylistsRaw(userConfigApi)
     # logger.info(f"Playlists: {playlists}")
     playlistsDerived = [
       DataLayerMapper.mapPlaylistRawToPlaylistDerived(playlist, userConfigApi)
-      for playlist in playlists
+      for playlist in playlistsRaw
     ]
     logger.info(f"Found {len(playlistsDerived)} raw playlists, and {len(playlistsDerived)} derived playlists.")
     # logger.info(f"Playlists (PlaylistDerived): {playlistsDerived}")
     return playlistsDerived
 
-
 @router.get("/{playlist_id}", response_model=PlaylistDerived)
 async def get_playlist(playlist_id: str):
     """Get single playlist with all songs"""
-    # find playlist
-    playlistUrl = UtilsSpotify.deriveSpotifyPlaylistUrlFromId(playlist_id)
-    logger.info(f"Derived playlist url: id -> url - {playlist_id} -> {playlistUrl}")
-    playlistRaw = next((
-      item 
-      for item in userConfigApi.config_as_object.saved_playlists
-      if str(item.url).startswith(playlistUrl)
-    ), None)
+    # find playlist by id
+    playlistRaw = UserConfigReaderApi.getPlaylistRaw(
+      playlist_id=playlist_id, 
+      userConfigApi=userConfigApi
+    )
     if not playlistRaw:
       logger.error(f"Playlist {playlist_id} not found")
       raise HTTPException(status_code=404, detail="Playlist not found")
     # derive PlaylistDerived
-    playlistDerived = DataLayerMapper.mapPlaylistRawToPlaylistDerived(playlistRaw, userConfigApi)
+    playlistDerived = DataLayerMapper.mapPlaylistRawToPlaylistDerived(
+      playlistRaw=playlistRaw, 
+      userConfigApi=userConfigApi
+    )
     return playlistDerived
   
-# @router.post("/{playlist_id}/edit", response_model=PlaylistDerived)
-# async def edit_playlist(playlist_id: str, payload: PlaylistEditRequest):
-#     """Edit playlist"""
-#     logger.info(f"Editing playlist {playlist_id}")
-#     prevPlaylistRaw = next((
-#       item 
-#       for item in userConfigApi.config_as_object.saved_playlists
-#       if str(item.url).startswith(playlist_id)
-#     ), None)
-#     if not prevPlaylistRaw:
-#       logger.error(f"Playlist {playlist_id} not found")
-#       raise HTTPException(status_code=404, detail="Playlist not found")
-#     # update playlist
-#     # TODO: Implement
-#     raise HTTPException(status_code=404, detail="Playlist not found")
+@router.post("/{playlist_id}/spotify/refetch", response_model=bool)
+async def refetch_spotify_side(playlist_id: str):
+    """Fetch fresh data from Spotify and merge with local config"""
+    logger.info(f"Refreshing playlist {playlist_id}")
+    
+    # ensure playlist exists in user config
+    oldPlaylistRaw = UserConfigReaderApi.getPlaylistRaw(playlist_id, userConfigApi)
+    if not oldPlaylistRaw:
+      logger.error(f"Playlist {playlist_id} not found in your config")
+      raise HTTPException(status_code=404, detail="Playlist not found in your config")
+    
+    # fetch updated playlist data from Spotify
+    freshPlaylistSpotifyData = UtilsSpotify.fetchSpotifyPlaylistTracksAndData(playlist_id)
+    if not freshPlaylistSpotifyData:
+      logger.error(f"Playlist {playlist_id} not found in Spotify")
+      raise HTTPException(status_code=404, detail="Playlist not found in Spotify but is in your config. Maybe you made the playlist private or deleted it from Spotify?")
+    
+    freshSpotifyPlaylistTracks = freshPlaylistSpotifyData[1]
+    # print(freshSpotifyPlaylistMeta)
+    # print(freshSpotifyPlaylistTracks[0])
+    
+    # create new raw data (for user config) 
+    newConfigTracks: list[TrackRaw] = []
+    for freshSpotifyTrack in freshSpotifyPlaylistTracks:
+      oldTrackInConfigData = UserConfigReaderApi.getTrackRaw(
+        playlist_id=playlist_id, 
+        track_id=freshSpotifyTrack.spotify_id, 
+        userConfigApi=userConfigApi
+      )
+      oldTrackInConfig = oldTrackInConfigData[0] if oldTrackInConfigData else None
+      newConfigTrack = TrackRaw(
+        spotify_id=freshSpotifyTrack.spotify_id,
+        title=freshSpotifyTrack.title,
+        artists=freshSpotifyTrack.artists,
+        album=freshSpotifyTrack.album or "",
+        release_date=freshSpotifyTrack.release_date or "",
+        duration_ms=freshSpotifyTrack.duration_ms or 0,
+        preview_url=freshSpotifyTrack.preview_url or "",
+        youtube_url=oldTrackInConfig.youtube_url if oldTrackInConfig else None,
+      )
+      logger.info(f"newConfigTrack: {newConfigTrack}")
+      newConfigTracks.append(newConfigTrack)
+      
+    # update playlist to user config
+    logger.info(f"json: {newConfigTracks}")
+    userConfigApi.update_playlist_tracks(playlist_id, newConfigTracks)
+    
+    return True
 
-# @router.post("/{playlist_id}/refresh", response_model=PlaylistDerived)
-# async def refresh_playlist(playlist_id: str):
-#     """Fetch fresh data from Spotify and merge with local config"""
-#     logger.info(f"Refreshing playlist {playlist_id}")
-#     # TODO: Implement
-#     raise HTTPException(status_code=404, detail="Playlist not found")
+@router.post("/edit-track")
+async def edit_track(request: PlaylistEditTrackPayload):
+    """Edit track in user config"""
+    logger.info(f"Editing track {request.track_id} of playlist {request.playlist_id}, request: {request}")
+    
+    # update
+    result = userConfigApi.update_playlist_track(request)
+    
+    # if track not found, 404
+    if result == None or result != True:
+      logger.error(f"Track {request.track_id} not found in playlist {request.playlist_id}")
+      raise HTTPException(status_code=404, detail="Track not found")
+    
+    return True
+    
+@router.post("/{playlist_id}/track/{track_id}/youtube/auto-search-url", response_model=bool)
+async def find_and_set_track_youtube_url(playlist_id: str, track_id: str):
+    """Find and set YouTube URL for a track"""
+    logger.info(f"Find YouTube URL for track {track_id}")
+    
+    # get track
+    trackRawData = UserConfigReaderApi.getTrackRaw(
+      playlist_id=playlist_id,
+      track_id=track_id,
+      userConfigApi=userConfigApi
+    )
+    if not trackRawData:
+      logger.error(f"Track {track_id} not found in playlist {playlist_id}")
+      raise HTTPException(status_code=404, detail="Track not found")
+    trackRaw = trackRawData[0]
+    
+    # find YouTube URL
+    youtubeUrl = UtilsYoutubeFetcherApi.findYoutubeUrlOfTrack(trackRaw)
+    logger.info(f"Found YouTube URL: {youtubeUrl}")
+    if not youtubeUrl:
+      logger.error(f"Could not find YouTube URL for track {track_id}")
+      raise HTTPException(status_code=500, detail="Could not find YouTube URL")
+    
+    # update track in config
+    updateResult = userConfigApi.update_playlist_track(PlaylistEditTrackPayload(
+      playlist_id=playlist_id,
+      track_id=track_id,
+      youtube_url=youtubeUrl
+    ))
+    if updateResult != True:
+      logger.error(f"Could not update Track {track_id} in playlist {playlist_id}")
+      raise HTTPException(status_code=500, detail="Cannot update track")
+    
+    return updateResult
 
+@router.get("/{playlist_id}/track/{track_id}/disk/get-audio-file", response_class=FileResponse)
+async def play_disk_track(playlist_id: str, track_id: str):
+    """Play track file from disk"""
+    logger.info(f"Play request for track {track_id}")
+    
+    # get track raw
+    trackRawData = UserConfigReaderApi.getTrackRaw(
+      playlist_id=playlist_id,
+      track_id=track_id,
+      userConfigApi=userConfigApi
+    )
+    if not trackRawData:
+      logger.error(f"Track {track_id} not found in playlist {playlist_id}")
+      raise HTTPException(status_code=404, detail="Track not found")
+    trackRaw, playlistRaw, trackRawIndex = trackRawData
+    
+    # derive track derived
+    trackDerived = DataLayerMapper.mapTrackRawToTrackDerived(
+      trackRaw=trackRaw,
+      playlistRaw=playlistRaw,
+      index=trackRawIndex,
+      userConfigApi=userConfigApi
+    )
+    
+    # return file
+    return FileResponse(
+      path=trackDerived.disk_file_path,
+      media_type="audio/mpeg",
+      filename="song.mp3",
+    )
+    
+@router.post("/{playlist_id}/track/{track_id}/disk/download", response_model=bool)
+async def download_track(playlist_id: str, track_id: str):
+    """Download track from YouTube as MP3 and save to disk"""
+    logger.info(f"Downloading track {track_id}")
+    
+    # get track raw
+    trackRawData = UserConfigReaderApi.getTrackRaw(
+      playlist_id=playlist_id,
+      track_id=track_id,
+      userConfigApi=userConfigApi
+    )
+    if not trackRawData:
+      logger.error(f"Track {track_id} not found in playlist {playlist_id}")
+      raise HTTPException(status_code=404, detail="Track not found")
+    trackRaw, playlistRaw, trackRawIndex = trackRawData
+    
+    # derive track derived
+    trackDerived = DataLayerMapper.mapTrackRawToTrackDerived(
+      trackRaw=trackRaw,
+      playlistRaw=playlistRaw,
+      index=trackRawIndex,
+      userConfigApi=userConfigApi
+    )
+    
+    # download track
+    UtilsYoutubeFetcherApi.downloadYoutubeTrackAsMp3(trackDerived)
+    return True
+  
+@router.post("/{playlist_id}/track/{track_id}/disk/delete-file", response_model=bool)
+async def delete_track(playlist_id: str, track_id: str):
+    """Delete track file from disk"""
+    logger.info(f"Delete request for track {track_id}")
+    
+    # get track raw
+    trackRawData = UserConfigReaderApi.getTrackRaw(
+      playlist_id=playlist_id,
+      track_id=track_id,
+      userConfigApi=userConfigApi
+    )
+    if not trackRawData:
+      logger.error(f"Track {track_id} not found in playlist {playlist_id}")
+      raise HTTPException(status_code=404, detail="Track not found")
+    trackRaw, playlistRaw, trackRawIndex = trackRawData
+    
+    # derive track derived
+    trackDerived = DataLayerMapper.mapTrackRawToTrackDerived(
+      trackRaw=trackRaw,
+      playlistRaw=playlistRaw,
+      index=trackRawIndex,
+      userConfigApi=userConfigApi
+    )
+    
+    # delete file
+    deletedResult = UtilsTrackDisk.deleteTrackFile(trackDerived)
+    
+    if deletedResult == "FILE_NOT_FOUND":
+      logger.error(f"Track {track_id} not found in playlist {playlist_id}")
+      raise HTTPException(status_code=404, detail="File not found in disk")
+    
+    if deletedResult == "FILE_DELETE_ERROR":
+      logger.error(f"Error deleting track {track_id} from playlist {playlist_id}")
+      raise HTTPException(status_code=500, detail="Error deleting file from disk")
+    
+    return True
+  
+  
+  
