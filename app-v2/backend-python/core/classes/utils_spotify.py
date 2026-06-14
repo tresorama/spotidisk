b3c620677e754a0daef13adbf44a45f7e0c31918
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-14 18:17:25 +0200

    refactor: be - UtilsSpotify - rewrite  `deriveSpotifyPlaylistIdFromUrl` and `deriveSpotifyPlaylistUrlFromId`

--- app-v2/backend-python/core/classes/utils_spotify.py
+++ app-v2/backend-python/core/classes/utils_spotify.py
@@ -4,13 +4,16 @@ from core.classes.utils_spotify_fetcher_api import SpotifyFetcherApi
 
 class UtilsSpotify:
   @staticmethod
-  def deriveSpotifyPlaylistIdFromUrl(spotify_playlist_url: str) -> str:
+  def deriveSpotifyPlaylistIdFromUrl(spotifyPlaylistUrl: str) -> str:
     # input: https://open.spotify.com/playlist/6anvql1OK0kBbmX5tyFWYz?si=dauxr8iKRGu9LVClCqi9xg
     # output: 6anvql1OK0kBbmX5tyFWYz
-    id = urlparse(spotify_playlist_url).path.split("/")[2]
+    id = urlparse(spotifyPlaylistUrl).path.split("/")[2]
     return id
   
   @staticmethod
+  def deriveSpotifyPlaylistUrlFromId(spotifyPlaylistId: str) -> str:
+    return f"https://open.spotify.com/playlist/{spotifyPlaylistId}"
+  
   @staticmethod
   def fetchSpotifyPlaylistTracksAndData(spotifyPlaylistId: str): 
     """Fetch playlist data from Spotify's embed page.
