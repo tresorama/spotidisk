659e234fc40c0be05bf2ee247b37905f13c1e28f
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-14 18:58:38 +0200

    chore: fe - components - playlist-actions - create `PlaylistActions` comp (WIP)

--- app-v2/frontend-react/src/components/playlist-actions.tsx
+++ app-v2/frontend-react/src/components/playlist-actions.tsx
@@ -1,31 +1,46 @@
+import { useMutationPlaylistRefetchSpotifySide } from "#/hooks/use-playlists";
 import type { DerivedPlaylist } from "#/lib/api-client/types";
 import { Button } from "./ui/button";
+import { TooltipEasy } from "./ui/tooltip-easy";
 
 export function PlaylistActions({
   playlist
 }: {
   playlist: DerivedPlaylist;
 }) {
+
+  const mutationPlaylistRefetchSpotifySide = useMutationPlaylistRefetchSpotifySide();
+
   return (
     <div className="flex flex-wrap justify-between gap-2">
       <div className="flex gap-[inherit]">
-        <Button variant="secondary">
-          Fetch from Spotify
-        </Button>
-        <Button variant="secondary">
-          Fetch from Spotify
-        </Button>
-        <Button variant="secondary">
-          Fetch from Spotify
-        </Button>
-        <Button variant="secondary">
-          Fetch from Spotify
-        </Button>
+        <TooltipEasy tooltipText="Refetch playlist data from Spotify (required when Spotify side is changed and you want to sync to it!)">
+          <Button
+            onClick={() => mutationPlaylistRefetchSpotifySide.mutate({
+              playlistId: playlist.spotify_id,
+              playlistName: playlist.name,
+            })}
+            disabled={mutationPlaylistRefetchSpotifySide.isPending}
+            isLoading={mutationPlaylistRefetchSpotifySide.isPending}
+            variant="secondary"
+          >
+            Fetch from Spotify
+          </Button>
+        </TooltipEasy>
       </div>
       <div className="flex gap-[inherit]">
         <Button
           variant="secondary"
-          render={<a href={playlist.spotify_url} target="_blank">View in Spotify</a>}
+          nativeButton={false}
+          render={(
+            <a
+              href={playlist.spotify_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View in Spotify
+            </a>
+          )}
         />
         <Button variant="secondary">
           Open download folder (TODO)
