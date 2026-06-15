e71b51972f77af93357fb16bfdb385484919078f
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-15 21:34:49 +0200

    chore: fe - components - ui - progress-bar - create `ProgressBar` comp

---
+++ app-v2/frontend-react/src/components/ui/progress-bar.tsx
@@ -0,0 +1,18 @@
+export function ProgressBar({
+  progress
+}: {
+  /** 0-1 range */
+  progress: number,
+}) {
+  return (
+    <div className="w-full flex gap-2 items-center">
+      <div className="flex-1 h-2 rounded-2xl bg-muted-foreground/10 border overflow-hidden">
+        <div
+          className="w-full h-full bg-primary rounded-[inherit] origin-left transition-transform duration-800 ease-in-out"
+          style={{ scale: `${progress} 1` }}
+        />
+      </div>
+      {progress * 100}%
+    </div>
+  );
+}
