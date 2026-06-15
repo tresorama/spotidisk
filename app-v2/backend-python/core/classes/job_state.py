ef52a15e2802cc0704e56e86b0233e9402a26967
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-15 21:36:54 +0200

    chore: be - JobState - create class

---
+++ app-v2/backend-python/core/classes/job_state.py
@@ -0,0 +1,25 @@
+from typing import Callable, Awaitable
+
+class JobState:
+  """Single Job Progress Object"""
+  def __init__(
+    self, 
+    title: str, 
+    totalStepCount: int,
+    jobFn: Callable[[], Awaitable[None]]
+  ):
+    self.title = title
+    self.stepsTotal = totalStepCount
+    self.stepsCurrentNum = 0
+    self.jobFn = jobFn
+    
+  def isRunning(self):
+    return self.stepsTotal > self.stepsCurrentNum
+  
+  def incrementStep(self):
+    self.stepsCurrentNum += 1
+  
+  def getProgress(self):
+    if self.isRunning(): return (self.stepsCurrentNum / self.stepsTotal)
+    else: return 1
+    
