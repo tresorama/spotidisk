027b5a7598798a6ed383ff16fa535346cc4a65de
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-15 23:38:28 +0200

    feat: be - JobsExecutor - triggering new Job cancel the old one

--- app-v2/backend-python/core/classes/jobs_executor.py
+++ app-v2/backend-python/core/classes/jobs_executor.py
@@ -1,5 +1,6 @@
 import asyncio
 from core.classes.job import Job
+from core.singleton.logger import logger
 
 class JobsExecutor:
   """In-Memory Job Executor. Used to execute jobs and share the current job state between threads and python functions"""
@@ -7,17 +8,24 @@ class JobsExecutor:
   task: None | asyncio.Task = None
   
   def getCurrentJob(self):
-    if not self.job:
-      return None
-    if not self.task:
-      return None
+    if not self.job:  return None
+    if not self.task: return None
     return (self.job, self.task)
   
   def setAndStartNewJob(self, job: Job):
     # cancel any existing job
     if self.task and not self.task.done():
+      logger.info("JobsExecutor - setAndStartNewJob - canceling existing job")
       self.task.cancel()
+      self.task.remove_done_callback(self.onTaskDone)
     # set new job
     self.job = job
     # start new job
     self.task = asyncio.create_task(job.jobFn(job))
+    # add listeners
+    self.task.add_done_callback(self.onTaskDone)
+    
+  def onTaskDone(self, task: asyncio.Task):
+    if self.task == task:
+      self.task = None
+      self.job = None
