e687d5d12a76e58294483a00bbb69697b268f54d
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-06-29 16:21:58 +0200

    feat: fe - change app name to "SpotiDisk"

--- app-v2/frontend-react/src/components/views/root/sidebar/index.tsx
+++ app-v2/frontend-react/src/components/views/root/sidebar/index.tsx
@@ -19,7 +19,7 @@ export function RootSidebar() {
   return (
     <Sidebar className="border-r">
       <SidebarHeader className="min-h-16 border-b px-6 py-2 flex flex-row items-center justify-between">
-        <Link to="/" className="text-xl font-semibold">sunnify</Link>
+        <Link to="/" className="text-xl font-semibold">SpotiDisk</Link>
         <DebugPanelToggler />
       </SidebarHeader>
       <SidebarContent>
