e3949a7561f6be7a48cde0bbb0acd2bb34b003a5
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-07-16 16:07:11 +0200

    chore: electron-builder - reorganiz logger code

--- app-v2-electron-builder/src/lib/logger.ts
+++ app-v2-electron-builder/src/lib/logger.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import nodeUtil from "node:util";
 
 // main class
 
@@ -28,47 +29,15 @@ export class Logger {
   }
 }
 
-// sub classes
+// transport
 
 export type LoggerTransport = {
   printLog: (key: string, ...args: Parameters<typeof console.log>) => void;
 };
 
-export class LoggerTransportConsole implements LoggerTransport {
-  color: Color;
-  constructor({
-    color
-  }: {
-    color: Color;
-  }) {
-    this.color = color;
-  }
-  printLog(key: string, ...args: Parameters<typeof console.log>) {
-    const keyNice = color[this.color](key);
-    const parts = [keyNice, ...args];
-    console.log(...parts);
-  }
-}
-export class LoggerTransportFile implements LoggerTransport {
-  filePath: string;
-  constructor(filePath: string) {
-    this.filePath = filePath;
-    this.createDirIfNotExists();
-  }
-  printLog(key: string, ...args: Parameters<typeof console.log>) {
-    const parts = [key, ...args];
-    fs.appendFileSync(this.filePath, parts.join(' ') + '\n');
-  }
-  private createDirIfNotExists() {
-    const dirPath = path.dirname(this.filePath);
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
-  }
-}
+// transport - console
 
-// utils
-const color = {
+const CONSOLE_COLOR_MAP = {
   red: (s: string) => `\x1b[31m${s}\x1b[0m`,
   green: (s: string) => `\x1b[32m${s}\x1b[0m`,
   yellow: (s: string) => `\x1b[33m${s}\x1b[0m`,
@@ -77,4 +46,54 @@ const color = {
   bold: (s: string) => `\x1b[1m${s}\x1b[0m`,
 };
 
-type Color = keyof typeof color;
+export class LoggerTransportConsole implements LoggerTransport {
+  color: keyof typeof CONSOLE_COLOR_MAP;
+  constructor({
+    color
+  }: {
+    color: keyof typeof CONSOLE_COLOR_MAP;
+  }) {
+    this.color = color;
+  }
+  printLog(key: string, ...args: Parameters<typeof console.log>) {
+    const keyNice = CONSOLE_COLOR_MAP[this.color](key);
+    const parts = [keyNice, ...args];
+    console.log(...parts);
+  }
+}
+
+// transport - file
+
+export class LoggerTransportFile implements LoggerTransport {
+  filePath: string;
+  fileStream: fs.WriteStream | null = null;
+  checkedIfDirExists: boolean = false;
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+  printLog(key: string, ...args: Parameters<typeof console.log>) {
+    const parts = [key, ...args];
+    const formattedMessage = nodeUtil.format(...parts) + '\n';
+    this.appendLogToFileStream(formattedMessage);
+  }
+  private appendLogToFileStream(textLine: string) {
+    // create dir if not exists
+    if (!this.checkedIfDirExists) {
+      this.checkedIfDirExists = true;
+      const dirPath = path.dirname(this.filePath);
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+      }
+    }
+    // create file stream if not exists
+    if (!this.fileStream) {
+      this.fileStream = fs.createWriteStream(this.filePath, { flags: 'a', encoding: 'utf8' });
+      this.fileStream.on('error', (err) => {
+        console.error(`[LoggerTransportFile] Errore di scrittura sul file ${this.filePath}:`, err);
+      });
+    }
+    // write to stream , that will be written to file
+    this.fileStream.write(textLine);
+  }
+}
+
