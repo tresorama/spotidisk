5107ba03861c44c6546f5d9ec62b652aae56820d
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-08-25 12:52:39 +0200

    chore: fe - components - ui - debug - create DebugOnlyTooltipData

--- app-v2/frontend-react/src/components/ui/debug.with-state.tsx
+++ app-v2/frontend-react/src/components/ui/debug.with-state.tsx
@@ -1,4 +1,6 @@
 import { useGlobalDebugVisibility } from "#/state/global.debug-visibility";
+import { InfoIcon } from "lucide-react";
+import { TooltipEasy } from "./tooltip-easy";
 
 /** React Component - Render children if debug mode is enabled */
 export function DebugOnly({ children }: { children: React.ReactNode; }) {
@@ -7,4 +9,22 @@ export function DebugOnly({ children }: { children: React.ReactNode; }) {
     return null;
   }
   return <>{children}</>;
+}
+
+
+export function DebugOnlyTooltipData({ data }: { data: unknown; }) {
+  return (
+    <DebugOnly>
+      <TooltipEasy
+        classNameContent="w-180 max-w-[90dvw] bg-muted-foreground"
+        tooltipText={(
+          <pre className="w-180 max-h-[90dvh] overflow-auto">
+            {JSON.stringify(data, null, 2)}
+          </pre>
+        )}
+      >
+        <InfoIcon className="size-[1em] text-muted-foreground" />
+      </TooltipEasy>
+    </DebugOnly>
+  );
 }
