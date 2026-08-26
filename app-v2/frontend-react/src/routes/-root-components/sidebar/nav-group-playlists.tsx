f8c4e5ff00a73564a6d842f10f09bcefb91970fa
Jacopo Marrone <jacopo.marrone27@gmail.com>
2026-08-26 17:32:03 +0200

    feat: fe - root-layout - sidebar - playlist - add "Delete" playlist feature (dropdown)

--- app-v2/frontend-react/src/routes/-root-components/sidebar/nav-group-playlists.tsx
+++ app-v2/frontend-react/src/routes/-root-components/sidebar/nav-group-playlists.tsx
@@ -1,11 +1,19 @@
-import { Link, useMatchRoute } from "@tanstack/react-router";
+import { Link, useMatchRoute, useNavigate } from "@tanstack/react-router";
+import { EllipsisIcon, TrashIcon } from "lucide-react";
 
-import { usePlaylists, type PlaylistRaw } from "#/data";
+import {
+  type PlaylistRaw,
+  usePlaylists,
+  useMutationPlaylistDeletePlaylist,
+} from "#/data";
 
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSkeleton } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Alert } from "@/components/ui/alert";
-import { DebugOnlyTooltipData } from "#/components/ui/debug.with-state";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DialogConfirmAction, useDialogConfirmAction } from "@/components/ui/dialog.confirm-action";
+import { DebugOnlyTooltipData } from "@/components/ui/debug.with-state";
 
 export function AppSidebarNavGroupPlaylists() {
   const queryPlaylists = usePlaylists();
@@ -48,8 +56,18 @@ function SidebarItemPlaylist({
 }: {
   playlist: PlaylistRaw;
 }) {
-  const matchRoute = useMatchRoute();
 
+  // data
+  const mutationDeletePlaylist = useMutationPlaylistDeletePlaylist();
+
+  // router
+  const matchRoute = useMatchRoute();
+  const navigate = useNavigate();
+
+  // local state
+  const dialogStateDeletePlaylist = useDialogConfirmAction();
+
+  // derived
   const isActive = Boolean(matchRoute({
     to: "/playlist/$playlistId",
     params: { playlistId: playlist.spotify_id },
@@ -57,23 +75,85 @@ function SidebarItemPlaylist({
     // fuzzy: !item.exact 
   }));
 
+  // events
+  const handleConfirmedDeletePlaylist = async () => {
+    if (isActive) {
+      navigate({ to: "/" });
+    }
+
+    await mutationDeletePlaylist.mutateAsync({
+      path: {
+        playlist_id: playlist.spotify_id,
+      }
+    });
+
+  };
+
   return (
     <SidebarMenuItem key={playlist.spotify_id}>
       <SidebarMenuButton
         isActive={isActive}
         render={
-          <Link
-            to="/playlist/$playlistId"
-            params={{ playlistId: playlist.spotify_id }}
-          >
-            <div className="flex-1 flex items-center gap-2">
-              <DebugOnlyTooltipData data={playlist} />
-              <span className="text-sm font-medium truncate">{playlist.name}</span>
+          <div className="group/item relative isolate flex-1 flex items-center gap-2">
+
+            <Link
+              to="/playlist/$playlistId"
+              params={{ playlistId: playlist.spotify_id }}
+              className="z-0 absolute inset-0"
+            />
+
+            <DebugOnlyTooltipData
+              data={playlist}
+              className="z-10 relative"
+            />
+
+            <span className="text-sm font-medium truncate select-none">
+              {playlist.name}
+            </span>
+
+            <div className="ml-auto empty:hidden flex items-center gap-[inherit]">
+
               {!playlist.lastSpotifyFetchDateTimeISO && (
-                <Badge className="ml-auto">NEW</Badge>
+                <Badge>NEW</Badge>
               )}
+
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  render={(
+                    <Button
+                      variant="secondary"
+                      size="icon-sm"
+                      className="z-10 relative -mr-2 opacity-0 group-hover/item:opacity-100"
+                    >
+                      <EllipsisIcon />
+                    </Button>
+                  )}
+                />
+                <DropdownMenuContent align="end">
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem
+                      onClick={() => dialogStateDeletePlaylist.setIsOpen(true)}
+                      variant="destructive"
+                    >
+                      <TrashIcon />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
             </div>
-          </Link>
+
+            <DialogConfirmAction
+              dialogState={dialogStateDeletePlaylist}
+              onConfirm={handleConfirmedDeletePlaylist}
+              title="Are you sure to delete this playlist?"
+              description="This action cannot be undone and cannot be reversed. Already downloaded tracks will not be deleted from your disk."
+              buttonCancelText="Keep it"
+              buttonConfirmText="Delete Playlist"
+            />
+
+          </div>
         }
       />
     </SidebarMenuItem>
